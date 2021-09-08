043d7e568756fdfb3cab499737cab9851aa79f22
Casey Stone <cstone@anl.gov>
2021-09-08 10:43:16 -0500

    initial commit

---
+++ sarah_protocol/template_gui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+def generate_button_click():
+    path_label = Label(root, )
+
+generate_button = Button(root, text="Generate Protocol")
+generate_button.grid(row=0, column=0)
+
+root.mainloop()
+
