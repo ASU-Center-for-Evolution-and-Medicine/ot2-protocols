719558afd9089ca15da13fbf370746d1af256280
Casey Stone <cstone@anl.gov>
2021-09-14 16:29:18 -0500

    working gui and protocol generation

--- sarah_protocol/template.py
+++ sarah_protocol/template.py
@@ -24,318 +24,57 @@ def write_protocol(protocol_path, wells, volumes):
                     if contents_this[i].startswith("### TD"):
                         open_that.write(f"\nsource_wells = {str(wells)}")
                         open_that.write(f"\nsource_volumes = {str(volumes)}\n")
-
+        
+        return(f"Protocol created = {protocol_path} ")
     except: 
-        print("ERROR: Could not write to protocol file")
+        return("Error: Could not write to protocol file")
 
+# MAIN METHOD --------------------------------------------------------------------
 
-# MAIN TEMPLATE CONTENTS ----------------------------------------------------------  
+def generate_from_template(source_csv_list, output_folder, file_name): 
+    source_csvs = source_csv_list
+    source_wells = {}
+    source_volumes = {} 
+    output = ""
+  
+    # extract data from csvs
+    tips = 0
+    for loc, path in source_csvs:  # this should still work
+        df = pd.read_csv(path, encoding='utf-8-sig')
+        wells = []
+        volumes = []
+        for each in df["Source"]: 
+            wells.append(each)
+        for each in df["Volume"]: 
+            volumes.append(each)
+        source_wells[loc] = wells
+        source_volumes[loc] = volumes
 
-# ask user to input source csv files (between 1 and 5 files allowed)
-source_csvs = []
-source_wells = {}
-source_volumes = {} 
-csv_input_count = 1   
-used_deck_loc = []
+        tips += len(source_wells[loc]) # keep track of number of tips needed
 
-# loop to gather source csvs
-print("\nEnter csv file paths and deck locations for source plates below (1-5 plates allowed)")
-print("-----------------------------------------------------")
-while True: 
-    current_path = input(f"CSV FILE PATH {csv_input_count} (type 'done' to continue): ").strip()
-    if current_path == 'done': 
-        break
-    elif not os.path.exists(current_path): 
-        print("Error: provided path does not exist")
-        continue
-    else: 
-        while True: 
-            current_deck_loc = input("DECK LOCATION (integer 2-6 inclusive): ") 
-            try: 
-                int_deck_loc = int(current_deck_loc.strip())
+    # determine the number of tips needed 
+    num_boxes = int(tips/96)+1 if not tips%96 == 0 else int(tips/96)
+    tip_box_loc = [7,8,9,10,11]
 
-                if int_deck_loc < 2 or int_deck_loc > 6: 
-                    print("Error: deck location must be an integer between 2 and 6 inclusive")
-                    continue
-                if int_deck_loc in used_deck_loc: 
-                    print(f"Error: deck location {int_deck_loc} already used")
-                    continue
-                break
+    # add deck layout to output string -----
+    # output += "DECK LAYOUT: "
+    # output += "1: DESTINATION PLATE"
+    # output += "SOURCE PLATES: "
+    # for loc, path in source_csvs: 
+    #     output += f"{loc}: {path}"
+    # output += "TIP BOXES: "
+    # for i in range(num_boxes): 
+    #     output += f"{tip_box_loc[i]}: 20uL tip box"
+    # --------------------------------------
 
-            except: 
-                print("Error: deck location must be an integer")
-                continue
-    
-    current_tuple = (int_deck_loc, current_path)
-    used_deck_loc.append(int_deck_loc)
-    source_csvs.append(current_tuple)
-
-    csv_input_count += 1
-    if csv_input_count == 6:
-        print("MAX NUMBER OF FILES REACHED")
-        break
-
-# extract data from csvs
-tips = 0
-for loc, path in source_csvs: 
-    df = pd.read_csv(path, encoding='utf-8-sig')
-    wells = []
-    volumes = []
-    for each in df["Source"]: 
-        wells.append(each)
-    for each in df["Volume"]: 
-        volumes.append(each)
-    source_wells[loc] = wells
-    source_volumes[loc] = volumes
-
-    tips += len(source_wells[loc]) # keep track of number of tips needed
-
-# determine the number of tips needed 
-num_boxes = int(tips/96)+1 if not tips%96 == 0 else int(tips/96)
-tip_box_loc = [7,8,9,10,11]
-
-# print deck layout -----
-print("\nDECK LAYOUT: ")
-print("\t1: DESTINATION PLATE")
-print("\tSOURCE PLATES: ")
-for loc, path in source_csvs: 
-    print(f"\t\t{loc}: {path}")
-print("\tTIP BOXES: ")
-for i in range(num_boxes): 
-    print(f"\t\t{tip_box_loc[i]}: 20uL tip box")
-# ------------------------
-
-if input("\nGenerate protocol now? (y/n): ").strip() == 'y':
-    # user input, where to write protocol
-    generate_folder = input("Path to folder that will store the generated file: ").strip()
-    protocol_filename = input("OT-2 protocol filename (exclude .py): ").strip() + str(".py")
-    # create output file path 
+    # where to write the protocol? # TODO: make this another GUI option
     try: 
-        file_to_create = os.path.join(generate_folder, protocol_filename)
-        write_protocol(file_to_create, source_wells, source_volumes)
-    except: 
-        print("Error: Coud not resolve output protocol file path")
+        file_to_create = os.path.join(output_folder, file_name)
+        write_output = write_protocol(file_to_create, source_wells, source_volumes)
+        output += write_output
+    except Error as e:  
+        output += f"\nError: Coud not resolve output protocol file path"
 
-quit()
+    return output
 
-
-
-### start  ------------------------------------------------------------------------------------------------------
-
-# metadata
-metadata = {
-'protocolName': 'Volume Transfer Protocol',
-'author': 'Casey Stone <cstone@anl.gov>',
-'description': 'Simple protocol to transfer a given set of volumes into a tube.',
-'apiLevel': '2.8'
-}
-
-### end
-
-### TD
-
-
-#This data structure is generated by the given csv. 
-#It informs the OT-2 about the source wells, destination wells, and the volumes.
-
-# exp_dict = { 
-#     "source_wells": df["Source"],
-#     "volumes": df["Volume"]
-#             }
-
-#print(list(zip(exp_dict["source_wells"], exp_dict["volumes"])))
-
-### start
-
-
-def run(protocol):
-    
-    #Defining the labware types and locations.
-    
-    PIPETTE_TYPE = "p20_single_gen2"
-    PIPETTE_MOUNT = "right"
-    
-    DESTINATION_PLATE_TYPE = "nest_96_wellplate_2ml_deep" #idk what plate you have, I just temp it up here.
-    DESTINATION_PLATE_SLOT = "1"
-    
-    SOURCE_PLATE_TYPE = "nest_96_wellplate_100ul_pcr_full_skirt" # same as above.
-    # SOURCE_PLATE_SLOT = "4"
-    
-    TIPRACK_TYPE = "opentrons_96_tiprack_20ul"
-    TIPRACK_SLOT1 = 7
-    TIPRACK_SLOT2 = 8
-    TIPRACK_SLOT3 = 9
-    TIPRACK_SLOT4 = 10
-    TIPRACK_SLOT5 = 11
-    
-    # TIPRACK_TYPE = "opentrons_96_tiprack_20ul"
-    # TIPRACK_SLOT = "5" #this is the slot you put the tiprack in.
-    INITIAL_TIP = "A1" #picks up the first tip from the upper left of rack. 
-    
-    
-    def transfer_volumes(source_wells, volumes):
-        
-        #load tip racks at all possible locations (don't need to actually use all these tip locations)
-        tiprack1 = protocol.load_labware(TIPRACK_TYPE, TIPRACK_SLOT1)
-        tiprack2 = protocol.load_labware(TIPRACK_TYPE, TIPRACK_SLOT2)
-        tiprack3 = protocol.load_labware(TIPRACK_TYPE, TIPRACK_SLOT3)
-        tiprack4 = protocol.load_labware(TIPRACK_TYPE, TIPRACK_SLOT4)
-        tiprack5 = protocol.load_labware(TIPRACK_TYPE, TIPRACK_SLOT5)
-
-        #load the pipette and point to location
-        pipette = protocol.load_instrument(PIPETTE_TYPE, mount=PIPETTE_MOUNT, tip_racks=[tiprack1, tiprack2, tiprack3, tiprack4, tiprack5])
-
-        print("PIPETTE TYPE:")
-        print(PIPETTE_TYPE)
-        print("TIP RACKS SLOTS:")
-        print(TIPRACK_SLOT1)
-        print(TIPRACK_SLOT2)
-        print(TIPRACK_SLOT3)
-        print(TIPRACK_SLOT4)
-        print(TIPRACK_SLOT5)
-        print("LOADED TIP RACKS")
-        print(tiprack1)
-        print(tiprack2)
-        print(tiprack3)
-        print(tiprack4)
-        print(tiprack5)
-
-        # format arrays of source names and locations
-        source_locs = []
-        for key in source_wells.keys(): 
-            source_locs.append(key)
-        # source_names = source_locs.copy()  # might not need this??
-        # source_names = [f"source{x}" for x in source_names]
-
-        source1 = None 
-        source2 = None
-        source3 = None 
-        source4 = None
-        source5 = None
-
-        source1_wells = None
-        source2_wells = None
-        source3_wells = None
-        source4_wells = None
-        source5_wells = None
-
-        source1_volumes = None
-        source2_volumes = None
-        source3_volumes = None 
-        source4_volumes = None 
-        source5_volumes = None 
-
-        # load the source plates and source wells
-        if len(source_locs) >= 1: 
-            source1 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[0])
-            source1_wells = [source1.wells_by_name()[well] for well in source_wells[source_locs[0]]]
-            source1_volumes = source_volumes[source_locs[0]]   # should be a list already :) 
-            
-            if len(source_locs) >= 2: 
-                source2 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[1])
-                source2_wells = [source2.wells_by_name()[well] for well in source_wells[source_locs[1]]]
-                source2_volumes = source_volumes[source_locs[1]]
-
-                if len(source_locs) >= 3: 
-                    source3 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[2])
-                    source3_wells = [source3.wells_by_name()[well] for well in source_wells[source_locs[2]]]
-                    source3_volumes = source_volumes[source_locs[2]]
-
-                    if len(source_locs) >= 4: 
-                        source4 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[3])
-                        source4_wells = [source4.wells_by_name()[well] for well in source_wells[source_locs[3]]]
-                        source4_volumes = source_volumes[source_locs[3]]
-
-                        if len(source_locs) == 5: 
-                            source5 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[4])
-                            source5_wells = [source5.wells_by_name()[well] for well in source_wells[source_locs[4]]]
-                            source5_volumes = source_volumes[source_locs[4]]
-
-                        elif len(source_locs) > 5: 
-                            print("Error: cannot load more than 5 source plates")
-                            raise
-        else: 
-            print("Error: there are no source locations to initialize")
-            raise
-
-        print("LOADED SOURCE PLATES")
-        print(source1)
-        print(f"\t{source1_wells}")
-        print(source2)
-        print(f"\t{source2_wells}")
-        print(source3)
-        print(f"\t{source3_wells}")
-        print(source4)
-        print(f"\t{source4_wells}")
-        print(source5)
-        print(f"\t{source5_wells}")
-
-        #load the destination plate (a 2ml tuberack in this case).
-        destination_plate = protocol.load_labware(DESTINATION_PLATE_TYPE, DESTINATION_PLATE_SLOT)
-        
-        #read in the destination wells on the destination plate(tuberack), in this case, just the first well since all volume goes into one well.
-        destination_well = destination_plate.wells()[0] #'A1'
-
-        #start the transfers, home the robot when transfers are complete
-        if source1: 
-            pipette.transfer(source1_volumes, source1_wells, destination_well, blowout=False, new_tip="always")
-        if source2: 
-            pipette.transfer(source2_volumes, source2_wells, destination_well, blowout=False, new_tip="always")
-        if source3: 
-            pipette.transfer(source3_volumes, source3_wells, destination_well, blowout=False, new_tip="always")
-        if source4: 
-            pipette.transfer(source4_volumes, source4_wells, destination_well, blowout=False, new_tip="always")
-        if source5: 
-            pipette.transfer(source5_volumes, source5_wells, destination_well, blowout=False, new_tip="always")
-        pipette.home()
-    
-    transfer_volumes(source_wells=source_wells, volumes=source_volumes)
-
-# run(protocol)
-
-# for line in protocol.commands():
-#     print(line)
-
-### end
-        
-# extra code snippets (DELETE LATER)
-
-
-        # # load the source plates  (find a cleaner way to do this)
-        # if len(source_locs) == 1: 
-        #     source1 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[0])
-        # elif len(source_locs) == 2: 
-        #     source1 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[0])
-        #     source2 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[1])
-        # elif len(source_locs) == 3: 
-        #     source1 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[0])
-        #     source2 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[1])
-        #     source3 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[2])
-        # elif len(source_locs) == 4: 
-        #     source1 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[0])
-        #     source2 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[1])
-        #     source3 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[2])
-        #     source4 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[3])
-        # elif len(source_locs) == 5: 
-        #     source1 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[0])
-        #     source2 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[1])
-        #     source3 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[2])
-        #     source4 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[3])
-        #     source5 = protocol.load_labware(SOURCE_PLATE_TYPE, source_locs[4])
-        # elif len(source_locs) < 1: 
-        #     print("Error: no source locations to initialize")
-        #     raise
-        # else: 
-        #     print("Error initializing source plates")
-        #     raise
-
-
-
-         # #load the source plate. 
-        # source_plate = protocol.load_labware(SOURCE_PLATE_TYPE, SOURCE_PLATE_SLOT)
-        
-        #load the source wells. 
-        #source_wells = [source_plate.wells_by_name()[well] for well in source_wells]
-        
-        #start the transfer, home the robot when it's done.
-        #pipette.transfer(list(volumes), source_wells, destination_well, blow_out=False, new_tip="always")
+    quit()    
--- sarah_protocol/template_gui.py
+++ sarah_protocol/template_gui.py
@@ -1,9 +1,13 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter import ttk
+import os
+from template import *
 
 root = Tk()
 root.title("Generate Transfer Protocol")
 
+# *CREATE WIDGETS -----------------------------------------------------------------------------
 # Labels
 csv_filename_label = Label(root, text="CSV FILENAME")
 deck_location_label = Label(root, text="DECK LOCATION")
@@ -13,6 +17,12 @@ source2_label = Label(root, text="Source 2: ")
 source3_label = Label(root, text="Source 3: ")
 source4_label = Label(root, text="Source 4: ")
 source5_label = Label(root, text="Source 5: ")
+separator = ttk.Separator(root, orient='horizontal')
+write_loc_label = Label(root, text="Output Folder: ")
+file_name_label = Label(root, text="File Name (without extension): ")
+
+output_label = Label(root, text = "")
+deck_info_label = Label(root, text= "", anchor=W)
 
 # Text Entry Boxes
 source1_text_entry = Entry(root, width=75)
@@ -21,43 +31,59 @@ source3_text_entry = Entry(root, width=75)
 source4_text_entry = Entry(root, width=75)
 source5_text_entry = Entry(root, width=75)
 
+write_loc_text_entry = Entry(root, width=75)
+write_loc_text_entry.insert(END, '/Users/cstone/Desktop/SarahTest')
+
+file_name_text_entry = Entry(root, width=75)
+file_name_text_entry.insert(END, "Protocol1")
+
 # Drop down boxes 
-drop1_loc = StringVar()
+drop1_loc = StringVar()  # set variable type
 drop2_loc = StringVar()
 drop3_loc = StringVar()
 drop4_loc = StringVar()
 drop5_loc = StringVar()
 
-drop1_loc.set(None)
+drop1_loc.set(None)  # set default value
 drop2_loc.set(None)
 drop3_loc.set(None)
 drop4_loc.set(None)
 drop5_loc.set(None)
 
-drop1 = OptionMenu(root, drop1_loc, None,2,3,4,5,6)
+drop1 = OptionMenu(root, drop1_loc, None,2,3,4,5,6)  # create drop downs
 drop2 = OptionMenu(root, drop2_loc, None,2,3,4,5,6)
 drop3 = OptionMenu(root, drop3_loc, None,2,3,4,5,6)
 drop4 = OptionMenu(root, drop4_loc, None,2,3,4,5,6)
 drop5 = OptionMenu(root, drop5_loc, None,2,3,4,5,6)
 
-
-#METHODS 
-def browse_click(browse_text_entry): 
-
-    csv_filename = filedialog.askopenfile(title = "Select file",filetypes = (("CSV Files","*.csv"),))
-
+# *HELPER METHODS ---------------------------------------------------------------------------
+def browse_csv_click(browse_text_entry): 
+    csv_filename = filedialog.askopenfile(title = "Select File",filetypes = (("CSV Files","*.csv"),))
     browse_text_entry.delete(0,"end")
     browse_text_entry.insert(0, csv_filename.name)
 
-def generate_button_click():
-    output_info_text = ""
-    output_error_text = ""
-    is_error = False
+def browse_folder_click(browse_text_entry): 
+    output_folder = filedialog.askdirectory(title="Select Folder")  # todo: add a default folfer opening location
+    browse_text_entry.delete(0,"end")
+    browse_text_entry.insert(0, output_folder)
 
-    # test section!
-    
-    # extract all deck locations from drop down boxes
 
+# *GENERATE PROTOCOL METHOD -----------------------------------------------------------------
+def generate_button_click(): 
+    global output_label
+    global deck_info_label
+    output_label.destroy()  # destroy the old output label 
+    deck_info_label.destroy()
+
+    output_label = Label(root, text="")  # instantiate new output label 
+    deck_info_label = Label(root, text="", anchor=W)  # instantiate new deck information label
+    is_error = False 
+    info_text = ""
+    error_text = ""
+    deck_text= ""
+    final_list = []
+
+    # extract deck locations from drop downs
     deck_locs = []
     verified_deck_locs = []
     repeats = False
@@ -67,6 +93,7 @@ def generate_button_click():
     deck_locs.append(drop4_loc.get())
     deck_locs.append(drop5_loc.get())
 
+    # ensure no repeated deck locations
     for i in range(len(deck_locs)): 
         if not deck_locs[i] == "None":  
             if len(verified_deck_locs) == 0: 
@@ -76,24 +103,10 @@ def generate_button_click():
                     verified_deck_locs.append(deck_locs[i])
                 else: 
                     repeats=True
-    print(deck_locs)
-    print(verified_deck_locs)
-    
+
     if repeats: 
         is_error = True
-        output_error_text = "Error: Please use a different deck location for each csv"
-    
-
-
-
-    
-
-    
-
-
-    
-
-
+        error_text += "\nError: Please use a different deck location for each csv"
 
     # extract all csv filenames from text entry boxes
     csv_filenames = []
@@ -110,57 +123,74 @@ def generate_button_click():
             csv_filenames[i] = csv_filenames[i].strip()
             if len(csv_filenames[i]) == 0: 
                 csv_filenames[i] = None
-            else: 
-                
-
-                num_csv += 1
         else: 
             csv_filenames[i] = None
 
-    output_info_text += f"\nNumber of CSVs processed = {num_csv}"
+    # check csv paths exist
+    for i in range(len(csv_filenames)):  
+        if csv_filenames[i]: 
+            if os.path.exists(csv_filenames[i]): 
+                num_csv += 1    
+            else: 
+                is_error = True
+                error_text += f"\npath dees not exist: {csv_filenames[i]}"
 
-    generate_error_label = Label(root, text=output_error_text)
-    generate_info_label = Label(root, text=output_info_text)
+            if not is_error: 
+                if not deck_locs[i] == "None":
+                    csv_tuple = (deck_locs[i], csv_filenames[i])
+                    final_list.append(csv_tuple)   # add to final list if all correct
+                else: 
+                    is_error = True
+                    error_text += f"\n Please enter a deck location for Source {i+1}"
 
-    if is_error: 
-        generate_error_label.grid(row=7, column=1, sticky=W)
+    # check that there are entries in final list 
+    if num_csv > 0: 
+        info_text += f"\nNumber of CSVs processed = {num_csv}"
     else: 
-        generate_info_label.grid(row=7, column=1, sticky=W)
+        is_error = True
+        error_text += f"\nNumber of CSVs processed = {num_csv}\nPlease enter a csv file and try again"
 
+    # check that output folder location exists 
+    output_loc = (write_loc_text_entry.get()).strip()
+    if not os.path.isdir(output_loc): 
+        is_error = True
+        error_text += "\nOutput Folder path does not exist"
+
+    # check that file name provided could be used to create valid file path # TODO: figure out how to do this
+    output_file_name = (file_name_text_entry.get()).strip()
     
+
+    # Decide whether or not to generate protocol 
+    if not is_error: 
+        more_info = generate_from_template(final_list, output_loc, output_file_name)  # generate the protocol
+
+        deck_text += more_info
+        output_label["text"] = info_text 
+        deck_info_label["text"] = deck_text
+
+        # gather the info about what protocol was created 
+    else: 
+        output_label["text"] = error_text  
+        
+    # display ouptut label from current click
+    output_label.grid(row=10, column=1,pady=3)
+    deck_info_label.grid(row=11, column=1, pady=3, padx=3)
     
-    #print(f"Source {i+1}: {csv_filenames[i]}")
-         
 
-
-
-    # check that the input is correct
-
-
-
-    # pass to template to make the protocol 
-
-
-
-    # output the location of the generated protoco (and instructions how to use it? )
-
-
-
-
-
-# Buttons
+# Buttons ------------------------------------------------------------------------------------
 generate_protocol_button = Button(root, text="Generate Protocol", command=generate_button_click)
 
-browse1 = Button(root, text="Browse", command=lambda:browse_click(source1_text_entry))
-browse2 = Button(root, text="Browse", command=lambda:browse_click(source2_text_entry))
-browse3 = Button(root, text="Browse", command=lambda:browse_click(source3_text_entry))
-browse4 = Button(root, text="Browse", command=lambda:browse_click(source4_text_entry))
-browse5 = Button(root, text="Browse", command=lambda:browse_click(source5_text_entry))
+browse1 = Button(root, text="Browse", command=lambda:browse_csv_click(source1_text_entry))
+browse2 = Button(root, text="Browse", command=lambda:browse_csv_click(source2_text_entry))
+browse3 = Button(root, text="Browse", command=lambda:browse_csv_click(source3_text_entry))
+browse4 = Button(root, text="Browse", command=lambda:browse_csv_click(source4_text_entry))
+browse5 = Button(root, text="Browse", command=lambda:browse_csv_click(source5_text_entry))
+write_loc_button = Button(root, text="Browse", command=lambda:browse_folder_click(write_loc_text_entry))
 
 
 # Load labels onto root 
-csv_filename_label.grid(row=0, column=1)
-deck_location_label.grid(row=0, column=3)
+csv_filename_label.grid(row=0, column=1, pady=3)
+deck_location_label.grid(row=0, column=3, padx=3, pady=3)
 
 source1_label.grid(row=1, column=0)
 source2_label.grid(row=2, column=0)
@@ -168,8 +198,13 @@ source3_label.grid(row=3, column=0)
 source4_label.grid(row=4, column=0)
 source5_label.grid(row=5, column=0)
 
+separator.grid(row=6, columnspan=4, sticky=EW, pady=10)
+
+write_loc_label.grid(row=7, column=0)
+file_name_label.grid(row=8, column=0, padx=3)
+
 # Load buttons onto root 
-generate_protocol_button.grid(row=6, column=1) 
+generate_protocol_button.grid(row=9, column=1, pady=5) 
 
 browse1.grid(row=1, column=2)
 browse2.grid(row=2, column=2)
@@ -177,12 +212,17 @@ browse3.grid(row=3, column=2)
 browse4.grid(row=4, column=2)
 browse5.grid(row=5, column=2)
 
+write_loc_button.grid(row=7, column=2)
+
 # Load text entry boxes onto root
 source1_text_entry.grid(row=1, column=1)
 source2_text_entry.grid(row=2, column=1)
 source3_text_entry.grid(row=3, column=1)
 source4_text_entry.grid(row=4, column=1)
 source5_text_entry.grid(row=5, column=1)
+
+write_loc_text_entry.grid(row=7, column=1)
+file_name_text_entry.grid(row=8, column=1)
 
 # Load drop down boxes onto root
 drop1.grid(row=1, column=3)
