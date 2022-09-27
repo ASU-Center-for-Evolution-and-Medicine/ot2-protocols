cd4ad0e799f65b348eef4406051741905888ee52
Stephanie <stephanie@biowl149.bio.anl.gov>
2022-09-27 17:50:16 -0500

    bug fix

--- sarah_protocol/template_gui.py
+++ sarah_protocol/template_gui.py
@@ -34,7 +34,7 @@ source5_text_entry = Entry(root, width=75)
 
 write_loc_text_entry = Entry(root, width=75)
 write_loc_text_entry.insert(END, '/Users/stephanie/Desktop/OT2_Protocols')
-write_loc_text_entry.insert(END, '/Users/cstone/Desktop/SarahTest') # LOCAL TESTING
+#write_loc_text_entry.insert(END, '/Users/cstone/Desktop/SarahTest') # LOCAL TESTING
 
 file_name_text_entry = Entry(root, width=75)
 file_name_text_entry.insert(END, "Protocol1")
